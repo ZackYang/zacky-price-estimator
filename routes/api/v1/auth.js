a834b5891de7c2b6678280a0126f6e9a194a2599
ZackYang <zackyang214@gmail.com>
2023-09-10 07:02:33 +1000

    Move auth route to api v1

---
+++ routes/api/v1/auth.js
@@ -0,0 +1,72 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const { checkSchema }  = require('express-validator');
+
+const AuthController = require('../../../controllers/api/v1/authController')
+const db = require('../../../models');
+const User = db.User;
+
+const checkIfEmailInUse = async (email) => {
+  const user = await User.findOne({where: { email: email}});
+  if (user) {
+    throw new Error('The Email is in use');
+  }
+};
+
+/* GET home page. */
+router.post(
+  '/register',
+  checkSchema({
+    email: {
+      trim: true,
+      notEmpty: true,
+      isEmail: true,
+      emailNotInUse: {
+        custom: checkIfEmailInUse
+      }
+    },
+    password: {
+      trim: true,
+      escape: true,
+      isStrongPassword: {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+      }
+    },
+    firstName: {
+      trim: true,
+      escape: true,
+      notEmpty: true,
+      isAlpha: true
+    },
+    lastName: {
+      trim: true,
+      escape: true,
+      notEmpty: true,
+      isAlpha: true
+    }
+  }),
+  AuthController.register
+);
+
+router.post(
+  '/login',
+  checkSchema({
+    email: {
+      trim: true,
+      notEmpty: true,
+      isEmail: true
+    },
+    password: {
+      trim: true,
+      escape: true,
+    }
+  }),
+  AuthController.login
+)
+
+module.exports = router;
