3b226fe9e48a1cf1f328352a62d43cc3c40058bf
ZackYang <zackyang214@gmail.com>
2023-09-11 13:52:12 +1000

    Add develop TOKEN_SECRET

--- controllers/api/v1/authController.js
+++ controllers/api/v1/authController.js
@@ -2,12 +2,14 @@
 const asyncHandler = require('express-async-handler');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const process = require('process');
 
 const db = require('../../../models');
 const User = db.User;
 
 const authenticatePassword = async (user, password) => {
-  if (!user) { return false };
+  if (!user) { return false }
 
   if (bcrypt.compareSync(password, user.hashedPassword)) {
     return true;
@@ -16,19 +18,24 @@ const authenticatePassword = async (user, password) => {
   return false;
 }
 
+const generateAccessToken = (email) => {
+  return jwt.sign(email, process.env.TOKEN_SECRET);
+}
+
 const findUserByEmail = async email => {
   const user = await User.findOne({ where: { email: email } });
   return user;
 }
 
-exports.register = asyncHandler(async (req, res, next) => {
+exports.register = asyncHandler(async (req, res) => {
   const result = validationResult(req);
   if (result.isEmpty()) {
     const user = await User.create({
       email: req.body.email,
       password: req.body.password,
       firstName: req.body.firstName,
-      lastName: req.body.lastName
+      lastName: req.body.lastName,
+      accessToken: generateAccessToken(req.body.email)
     });
 
     return res.send(`Hello, ${user.firstName}!`);
@@ -37,16 +44,17 @@ exports.register = asyncHandler(async (req, res, next) => {
   res.send({ errors: result.array() });
 });
 
-exports.login =  asyncHandler(async (req, res, next) => {
+exports.login =  asyncHandler(async (req, res) => {
   const user = await findUserByEmail(req.body.email);
-    console.log(user);
     if (await authenticatePassword(user, req.body.password)) {
-      return res.json(user);
+      user.accessToken = generateAccessToken(req.body.email);
+      await user.save();
+      return res.status(200).json({ accessToken: user.accessToken });
     }
 
     res.status(401).json({ message: "Email or Password is incorrect." });
 });
 
-const logout = (req, res, next) => {
+exports.logout = (req, res) => {
 
 };
